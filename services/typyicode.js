a223ff35f7e210abdcad9e0c0715372d8aefb502
charbel.f <charbel.f@i-stay.ltd>
2023-06-03 11:29:55 +0300

    F

--- services/typyicode.js
+++ services/typyicode.js
@@ -7,7 +7,32 @@ const getTypyicode = async () => {
     try {
         const URL = `https://jsonplaceholder.typicode.com/posts`;
         const result = await axios.get(URL);
-        console.log(result);
+
+        const posts = result.data;
+
+
+        let startTime = new Date().getTime(); //ms
+
+        for (let n = 0; n < 100; n++) {
+            let sql = ``;
+            for (let i = 0; i < posts.length; i++) {
+                const post = posts[i];
+                const { userId, id, title } = post;
+
+                sql += `INSERT INTO typicode (user_id, id, title, body) VALUES ("${userId}", "${id}", "${title}", "${post.body}")`;
+                
+                const dbres = await db.query(sql, [userId, id, title, post.body]);
+                console.log(dbres);
+            }
+        }
+
+        let endTime = new Date().getTime();
+
+        let elapsedTime = endTime - startTime;
+
+        console.log(elapsedTime);
+
+
     } catch (error) {
         console.log(error);
     }
