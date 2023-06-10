da4057157d5bbe5b6ef968f518ab0cfafff9f01b
charbel.f <charbel.f@i-stay.ltd>
2023-06-10 10:48:30 +0300

    sequalize example client

---
+++ model/Client.js
@@ -0,0 +1,51 @@
+const {DataTypes} = require("sequelize");
+const sequelize = require("../DB/configSqlz");
+
+
+const Client = sequelize.define('client', {
+    client_id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    client_first_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    client_last_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    client_username:{
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    client_email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    client_mobile:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    client_password:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    client_address:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    client_gender:{
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+    },
+    client_dob:{
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+        //defaultValue: DataTypes.NOW,
+    },
+});
+
+module.exports = Client;
---
+++ services/clientsSqlz.js
@@ -0,0 +1,99 @@
+// CRUD
+const Client = require("../model/Client");
+
+// create client
+const createClient = async ({firstName, lastName, username, password, email, mobile, address, gender, dob}) =>{
+
+    try{
+        const newClient = await Client.create({firstName, lastName, username, password, email, mobile, address, gender, dob});
+        return newClient.toJSON();
+    }catch(e){
+        console.error('Error creating client', error);
+    }
+
+}
+
+const getAllClients = async () =>{
+    try{
+        const clients = await Client.findAll();
+        return clients;
+    }catch(e){
+        console.error(e);
+    }
+}
+
+const getClientById = async (id) =>{
+    try{
+        const client = await Client.findByPk(id);
+        if(client){
+            return client;
+        }
+        return "user not found";
+    }catch(error){
+        console.error(error);
+    }
+}
+
+const updateClient = async (id, firstName, lastName, username, password, email, mobile, address, gender, dob) => {
+    try{
+        // fetched the user to update
+        const client = await Client.findByPk(id);
+        
+        if(client){
+            
+            client.firstName = firstName;
+            client.lastName = lastName,
+            client.username = username,
+            client.password = password;
+            client.email = email;
+            client.mobile = mobile;
+            client.address = address,
+            client.gender = gender;
+            client.dob = dob;
+
+            const updatedClient = await client.save();
+            return updatedClient.toJSON();
+        }
+    }catch(error){
+        console.error(error);
+    }
+}
+
+const deleteClient = async(id) =>{
+    try{
+        const client = await Client.findByPk(id);
+        if(client){
+            const deletedClient = await client.destroy();
+            return deletedClient.toJSON();
+        }
+    }catch(error){
+        console.error(error);
+    }
+}
+
+const authenticateClient = async (username, password) => {
+    try{
+        const client = await Client.findOne({where: {client_username: username}});
+        if(client){
+            if(client.password === password){
+                return client.toJSON();
+            }
+            return "Not authenticated";
+        }
+
+        return "Client Not found";
+
+    }catch(error){
+        console.error(error);
+    }
+}
+
+module.exports = {
+    createClient,
+    getAllClients,
+    getClientById,
+    updateClient,
+    deleteClient,
+    authenticateClient,
+}
+
